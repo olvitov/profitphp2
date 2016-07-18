480e1f1d771022faa3c7f55de99e69443b3bf358
olvit <olvitov@gmail.com>
2016-07-18 22:32:22 +0300

    Старт, добавили autoload.php

---
+++ app/Db.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: olvit
+ * Date: 18.07.2016
+ * Time: 22:23
+ */
+
+namespace App;
+
+
+class Db
+{
+   public function __construct()
+    {
+        echo 'Hello, DB!';
+    }
+}
